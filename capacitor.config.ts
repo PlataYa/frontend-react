462537e2772b8ec115807f44e639d8dfbdf0647c
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-06-12 02:20:06 -0300

    capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plataya.app',
+  appName: 'PlataYaApp',
+  webDir: 'build'
+};
+
+export default config;
